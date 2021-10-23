e40a2a9b750bc9d5292d9f3447ae0a0a9edb2cf0
Ashutosh Hans <ashutosh.hans@gmail.com>
2021-10-23 10:10:31 +0530

    auth set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if(_token) setToken(_token);
+  }, []);
+
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <h1>You are now logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
+      
     </div>
   );
 }
