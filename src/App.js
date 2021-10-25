67c6945d48a2ca4b1d34590056a4671a307dfeaf
Ashutosh Hans <ashutosh.hans@gmail.com>
2021-10-25 16:07:13 +0530

    structure set up and data layer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-
-  const [token, setToken] = useState(null);
+  const [{ user, token }, disptach] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-    if(_token) setToken(_token);
+    if(_token) {
+      disptach({
+        type: "SET_TOKEN",
+        token: _token
+      });
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        disptach({
+          type: "SET_USER",
+          user
+        })
+      })
+    }
   }, []);
 
   return (
     <div className="app">
       {
         token ? (
-          <h1>You are now logged in</h1>
+          <Player spotify={spotify}/>
         ) : (
           <Login />
         )
---
+++ src/reducer.js
@@ -0,0 +1,29 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  playing: false,
+  item: null,
+  // remove the token after development
+  token: "BQBtAkqnFRKcL_5P_qPDxGbMxqGhtnrTCixCDXbIQqr7mGJdw3gw0OKUmj5qjypH9k2yR_JQXt3lW5KKVy3ymUkhaYKC7PlGb3C-xWCzn521byddUdX00mbz2yOF7b3Yum8PKsP87Y9YGXQzvJypI9xs76z90Wq0CC9JyWsvFJqP1cBX"
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch(action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user
+      }
+    case "SET_TOKEN": 
+      return {
+        ...state,
+        token: action.token
+      }
+    default: 
+      return state;
+  }
+};
+
+export default reducer;
+
